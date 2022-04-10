48ab88ccd3f0cb374a7a1531cb9f5e5718116b00
Maghawry <mag.hauwry@gmail.com>
2022-04-11 00:39:54 +0200

    Images API Unslash added to api folder.

---
+++ src/api/unslash.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { apiKey } from './apiKey';
+
+export default axios.create({
+      baseURL: 'https://api.unsplash.com/',
+      headers: {
+        Authorization: apiKey // <- to play arround this repo to have to have your own apiKey; Unsplash developers account
+      }
+    }); 
+
+    // export const apiKey = 'Client-ID [your key] see api documentation for me info';
+
