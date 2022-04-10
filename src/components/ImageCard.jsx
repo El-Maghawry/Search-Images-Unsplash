cb5f5f4d6c6d9c02379b0d7153a5de1d69bb2f40
Maghawry <mag.hauwry@gmail.com>
2022-04-11 00:43:49 +0200

    ImageCard component added to components folder.

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.imageRef = React.createRef();
+
+        this.state = { spans: 0 };
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef)
+        console.log(this.imageRef.current.alt)
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans });
+    }
+
+    render(){
+        const { alt_description, urls } = this.props.image;
+
+        return(
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img 
+                    src={urls.regular}
+                    alt={alt_description}
+                    ref={this.imageRef}
+                />
+            </div>
+        )
+    }
+}
+
+export default ImageCard;
